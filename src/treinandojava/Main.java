930c14fe204c9a41114f3a43b36ffade3195f97f
Lincon Mello <lincon.mello.lm@gmail.com>
2025-11-25 21:23:17 -0300

    incluindo scanner

--- src/treinandojava/Main.java
+++ src/treinandojava/Main.java
@@ -1,8 +1,12 @@
 package treinandojava;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        Scanner teclado = new Scanner(System.in);
 
         Pessoa p1 = new Pessoa();
         Pessoa p2 = new Aluno();
@@ -10,8 +14,14 @@ public class Main {
 
         Pessoa[] pessoa = {p1, p2, p3};
 
+
         for (Pessoa correr : pessoa) {
             correr.andar();
         }
+        System.out.println("Digite seu nome: ");
+        String nome = teclado.nextLine();
+
+        System.out.println("\nNome digitado: " + nome);
+
     }
 }
